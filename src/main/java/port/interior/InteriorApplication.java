7cf13d7ae775bbcd3e3cdecf502c7009603b097e
shion002 <shion002002@gmail.com>
2025-02-27 13:14:21 +0900

    fix: log add

--- src/main/java/port/interior/InteriorApplication.java
+++ src/main/java/port/interior/InteriorApplication.java
@@ -8,6 +8,12 @@ public class InteriorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InteriorApplication.class, args);
+
+		// 환경 변수 출력 (서버 로그에서 확인 가능)
+		System.out.println("AWS_REGION: " + System.getenv("S3_REGION"));
+		System.out.println("AWS_BUCKET: " + System.getenv("S3_BUCKET_NAME"));
+		System.out.println("AWS_ACCESS_KEY: " + System.getenv("AWS_ACCESS_KEY"));
+		System.out.println("AWS_SECRET_KEY: " + System.getenv("AWS_SECRET_KEY"));
 	}
 
 }
