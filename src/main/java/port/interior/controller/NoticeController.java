72385f8b7bf26743f29cbed08f69b7d6fcbd9550
shion002 <shion002002@gmail.com>
2025-03-18 15:23:42 +0900

    clear

--- src/main/java/port/interior/controller/NoticeController.java
+++ src/main/java/port/interior/controller/NoticeController.java
@@ -151,6 +151,7 @@ public class NoticeController {
 
     @GetMapping("/get/notice")
     public List<NoticeResponseDto> getNoticeAll(){
+        noticeService.clear();
         return noticeService.findAll();
     }
 }
